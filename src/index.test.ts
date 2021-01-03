e9672c38cfd96fb222edb724f04afb97bb01d9b1
axemac <74874171+ax-lab@users.noreply.github.com>
2021-01-03 17:35:38 -0300

    Cleanup code

--- src/index.test.ts
+++
@@ -1,7 +0,0 @@
-import { expect, test } from '@jest/globals'
-
-import { answer } from './index'
-
-test('answer to everything should be 42', () => {
-	expect(answer()).toBe(42)
-})
--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,4 @@
 export * from './chars'
+export * from './hiragana'
 export * from './katakana'
-
-function answer() {
-	return 42
-}
-
-export { answer }
-
-import { to_katakana } from './katakana'
-console.log(to_katakana('かたかな　ありがとう'))
+export * from './romaji'
